b610c8888f11a572ef1797b02c97662ad75e6d08
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-03-21 12:53:30 +0800

    Add new file

---
+++ password.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 21 12:08:27 2021
+
+@author: yuanke.tsai
+"""
+
+remain_c = 3
+while remain_c > 0:
+    pass_w = input('請輸入密碼：')
+    if pass_w == 'a1234':
+        print('登入成功！')
+        break
+    remain_c = remain_c - 1
+    while remain_c >= 0:
+        if remain_c > 0:
+            print('try again! remain chance:', remain_c)
+            break
+        else:
+            print('try later!')
+            break
